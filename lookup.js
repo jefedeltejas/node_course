ec7fb27cc64b7a2f8dcdbbd9501c5f7facb14060
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-13 16:40:17 -0500

    a little fiddling

--- lookup.js
+++ lookup.js
@@ -2,6 +2,6 @@ console.log('Starting lookup app.');
 
 const dns = require('dns');
 
-dns.lookup('www.dashiell.com', (err, addresses, family) => {
+dns.lookup('npmjs.org', (err, addresses, family) => {
   console.log('addresses:', addresses);
 });
